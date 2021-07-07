6f0780ff29946b52814150dfec1d1d1a0109ecef
Tomic-Riedel <64801984+Tomic-Riedel@users.noreply.github.com>
2021-07-08 01:16:57 +0200

    Update scaffold.dart
    Created a separate widget instead of a widget function -> performance improvements, bug fixes

--- lib/src/scaffold.dart
+++ lib/src/scaffold.dart
@@ -1,61 +1,90 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-Widget ScaffoldGradientBackground({
-  required Gradient gradient,
-  Key? key,
-  PreferredSizeWidget? appBar,
-  Widget? body,
-  Widget? floatingActionButton,
-  FloatingActionButtonLocation? floatingActionButtonLocation,
-  FloatingActionButtonAnimator? floatingActionButtonAnimator,
-  List<Widget>? persistentFooterButtons,
-  Widget? drawer,
-  void Function(bool)? onDrawerChanged,
-  Widget? endDrawer,
-  void Function(bool)? onEndDrawerChanged,
-  Widget? bottomNavigationBar,
-  Widget? bottomSheet,
-  bool? resizeToAvoidBottomInset,
-  bool primary = true,
-  DragStartBehavior drawerDragStartBehavior = DragStartBehavior.start,
-  bool extendBody = false,
-  bool extendBodyBehingAppBar = false,
-  Color? drawerScrimColor,
-  double? drawerEdgeDragWidth,
-  bool drawerEnableOpenDragGesture = true,
-  bool endDrawerEnableOpenDragGesture = true,
-  String? restorationId,
-}) {
-  return Container(
-    decoration: BoxDecoration(
-      gradient: gradient,
-    ),
-    child: Scaffold(
-      key: key ?? null,
-      appBar: appBar ?? null,
-      body: body ?? null,
-      floatingActionButton: floatingActionButton ?? null,
-      floatingActionButtonLocation: floatingActionButtonLocation ?? null,
-      floatingActionButtonAnimator: floatingActionButtonAnimator ?? null,
-      persistentFooterButtons: persistentFooterButtons ?? null,
-      drawer: drawer ?? null,
-      onDrawerChanged: onDrawerChanged ?? null,
-      endDrawer: endDrawer ?? null,
-      onEndDrawerChanged: onEndDrawerChanged ?? null,
-      bottomNavigationBar: bottomNavigationBar ?? null,
-      bottomSheet: bottomSheet ?? null,
-      backgroundColor: Colors.transparent,
-      resizeToAvoidBottomInset: resizeToAvoidBottomInset ?? null,
-      primary: primary,
-      drawerDragStartBehavior: drawerDragStartBehavior,
-      extendBody: extendBody,
-      extendBodyBehindAppBar: extendBodyBehingAppBar,
-      drawerScrimColor: drawerScrimColor ?? null,
-      drawerEdgeDragWidth: drawerEdgeDragWidth ?? null,
-      drawerEnableOpenDragGesture: drawerEnableOpenDragGesture,
-      endDrawerEnableOpenDragGesture: endDrawerEnableOpenDragGesture,
-      restorationId: restorationId ?? null,
-    ),
-  );
+class ScaffoldGradientBackground extends StatelessWidget {
+  final Gradient gradient;
+  final PreferredSizeWidget? appBar;
+  final Widget? body;
+  final Widget? floatingActionButton;
+  final Widget? drawer;
+  final Widget? endDrawer;
+  final Widget? bottomNavigationBar;
+  final Widget? bottomSheet;
+  final FloatingActionButtonLocation? floatingActionButtonLocation;
+  final FloatingActionButtonAnimator? floatingActionButtonAnimator;
+  final List<Widget>? persistentFooterButtons;
+  final void Function(bool)? onDrawerChanged, onEndDrawerChanged;
+  final bool? resizeToAvoidBottomInset;
+  final bool primary;
+  final bool extendBody;
+  final bool extendBodyBehindAppBar;
+  final bool drawerEnableOpenDragGesture;
+  final bool endDrawerEnableOpenDragGesture;
+  final DragStartBehavior drawerDragStartBehavior;
+  final Color? drawerScrimColor;
+  final double? drawerEdgeDragWidth;
+  final String? restorationId;
+
+  const ScaffoldGradientBackground({
+    required this.gradient,
+    Key? key,
+    this.appBar,
+    this.body,
+    this.floatingActionButton,
+    this.floatingActionButtonLocation,
+    this.floatingActionButtonAnimator,
+    this.persistentFooterButtons,
+    this.drawer,
+    this.onDrawerChanged,
+    this.endDrawer,
+    this.onEndDrawerChanged,
+    this.bottomNavigationBar,
+    this.bottomSheet,
+    this.resizeToAvoidBottomInset,
+    this.primary = true,
+    this.drawerDragStartBehavior = DragStartBehavior.start,
+    this.extendBody = false,
+    this.extendBodyBehindAppBar = false,
+    this.drawerScrimColor,
+    this.drawerEdgeDragWidth,
+    this.drawerEnableOpenDragGesture = true,
+    this.endDrawerEnableOpenDragGesture = true,
+    this.restorationId,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: gradient,
+      ),
+      child: Scaffold(
+        key: key,
+        appBar: appBar,
+        body: body,
+        floatingActionButton: floatingActionButton,
+        floatingActionButtonLocation: floatingActionButtonLocation,
+        floatingActionButtonAnimator: floatingActionButtonAnimator,
+        persistentFooterButtons: persistentFooterButtons,
+        drawer: drawer,
+        onDrawerChanged: onDrawerChanged,
+        endDrawer: endDrawer,
+        onEndDrawerChanged: onEndDrawerChanged,
+        bottomNavigationBar: bottomNavigationBar,
+        bottomSheet: bottomSheet,
+        backgroundColor: Colors.transparent,
+        resizeToAvoidBottomInset: resizeToAvoidBottomInset,
+        primary: primary,
+        drawerDragStartBehavior: drawerDragStartBehavior,
+        extendBody: extendBody,
+        extendBodyBehindAppBar: extendBodyBehindAppBar,
+        drawerScrimColor: drawerScrimColor,
+        drawerEdgeDragWidth: drawerEdgeDragWidth,
+        drawerEnableOpenDragGesture: drawerEnableOpenDragGesture,
+        endDrawerEnableOpenDragGesture: endDrawerEnableOpenDragGesture,
+        restorationId: restorationId,
+      ),
+    );
+  }
 }
+
